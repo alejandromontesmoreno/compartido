0f3bf6d62e4d830ee5c3d5ffddc072623e625a9e
Alejandro Montes <alejandromontesmoreno@gmail.com>
2025-06-22 20:43:52 -0600

    Versión con github

--- programa1.py
+++ programa1.py
@@ -1,10 +1,8 @@
 print('Programa 1')
 print('Linea 1')
 print('Linea 2')
-<<<<<<< HEAD
 print('Linea 3 de master')
-=======
 print('Linea 3 rama1')
 
 
->>>>>>> rama1
+
--- programa2.py
+++ programa2.py
@@ -1,2 +1,3 @@
 print('Programa 2')
 print('Línea 1')
+print('Línea 2 en github')
